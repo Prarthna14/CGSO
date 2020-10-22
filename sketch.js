fbcee09234ca88cc9d3a0f1a901856df08990b67
Prarthna14 <72306062+Prarthna14@users.noreply.github.com>
2020-10-22 14:51:51 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,32 @@
+var car;
+var wall;
+var speed;
+var weight;
+var deformation=0;
+
+function setup() {
+  createCanvas(1600,400);
+  
+  speed=random(55,90);
+  weight=random(400,1500);
+  car=createSprite(50,200,50,50);
+  wall=createSprite(1500,200,60,height/2);
+  car.velocityX=speed;
+  deformation=(0.5*weight*speed*speed)/22500;
+}
+
+function draw() {
+  background(255,255,255);  
+if(car.collide(wall)){
+  if(deformation<100){
+    car.shapeColor="green";
+  }
+  if(deformation>100||deformation<180){
+    car.shapeColor="yellow";
+  }
+  if(deformation>180){
+    car.shapeColor="red";
+  }
+}
+  drawSprites();
+}
